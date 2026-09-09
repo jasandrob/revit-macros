5c283496e3e924abf30fcc0cf8c021234e7f6535
JJ HAWAIIAN <jasonroberts671@gmail.com>
2026-09-09 16:17:31 -0700

    added natural sort order to checklist, lists master directory

--- TEI.extension/TEI(test).tab/Testing.Panel/updateblocks.pushbutton/script.py
+++ TEI.extension/TEI(test).tab/Testing.Panel/updateblocks.pushbutton/script.py
@@ -3,10 +3,11 @@
 updates blocks
 """
 import os
+import re
 from pyrevit import revit, forms
 from Autodesk.Revit.DB import FilteredElementCollector, CADLinkType, ExternalFileUtils, ModelPathUtils, ImportInstance
 
-def main():
+def select_blocks():
     doc = revit.doc
     uidoc = revit.uidoc
 
@@ -14,7 +15,6 @@ def main():
     active_view_id = doc.ActiveView.Id
     
     # Collect all CAD Link Types in the model
-    #link_types = FilteredElementCollector(doc).OfClass(CADLinkType)
     link_types = FilteredElementCollector(doc, active_view_id).OfClass(ImportInstance)
 
     # Filter links that start with "DP_" or "DM_"
@@ -35,17 +35,9 @@ def main():
                     
                     if name.startswith("DP_") or name.startswith("DM_"):
                         # Map name to the instance element so we can select it on the sheet
-                        matching_links[name] = link_type   
+                        matching_links[name] = link_type, cad_link_str   
 
     
-    
-        # #get the file path/name as a string
-        # cad_link_ref = link.GetExternalFileReference().GetPath()
-        # cad_link_str = ModelPathUtils.ConvertModelPathToUserVisiblePath(cad_link_ref)
-        # name = os.path.basename(cad_link_str)
-
-        # if name.startswith("DP_") or name.startswith("DM_"):
-            # matching_links[name] = link
 
     if not matching_links:
         forms.alert(
@@ -57,25 +49,55 @@ def main():
 
     # Display a multi-select dialog box using pyRevit forms
     selected_names = forms.SelectFromList.show(
-        sorted(matching_links.keys()),
+        sorted(matching_links.keys(), key=natural_sort_key),
         multiselect=True,
-        title="Select Linked DWGs (DP_ / DM_)",
-        button_name="Select in Model"
+        title="Select Blocks in Model",
+        button_name="Update Blocks"
     )
 
     # If the user made a selection, update Revit's active selection
     if selected_names:
-        selected_elements = [matching_links[name] for name in selected_names]
-        element_ids = [el.Id for el in selected_elements]
+
+        selected_links = [matching_links[name][1] for name in selected_names]
         
-        # Set the selection in the active UI document
-        uidoc.Selection.SetElementIds(element_ids)
+        # selected_elements = [matching_links[name] for name in selected_names]
+        # element_ids = [el.Id for el in selected_elements]
         
-        forms.alert(
-            "Successfully selected {} linked DWG(s).".format(len(element_ids)), 
-            title="Done", 
-            exitscript=False
-        )
+        # # Set the selection in the active UI document
+        # uidoc.Selection.SetElementIds(element_ids)
+        
+        # forms.alert(
+            # "Successfully selected {} linked DWG(s).".format(len(element_ids)), 
+            # title="Done", 
+            # exitscript=False
+        # )
+
+def check_master():
+#creates a list of all the details in the sync revit directory
+
+    sync_path = "S:\MECHANICAL\_CostcoDetails\_SyncRevitDetails"
+    
+    all_items = os.listdir(sync_path)
+    
+    #get all dwgs
+    dwg_files = [
+        f for f in all_items
+        if os.path.isfile(os.path.join(sync_path, f)) and f.lower().endswith('.dwg')
+    ]
+    
+    for file in dwg_files:
+        print(file)
+
+
+def natural_sort_key(s):
+    # Splits the string into text and integer chunks
+    return [int(text) if text.isdigit() else text.lower() for text in re.split(r'(\d+)', s)]
+    
+    
+def main():
+    select_blocks()
+    check_master()
+
 
 if __name__ == "__main__":
     main()
